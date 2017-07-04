324e4a70b71238de41bfda0d1306ce075759647e
TK <tomaszkwiatkowski90@gmail.com>
2017-07-04 18:35:10 +0200

    zatwierdzone

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Pole Trójk¹ta
+
+//deklaracja zmiennych
+var     a=prompt('a=?'),
+	h=prompt('h=?'),
+	triangleArea;
+
+//wzór na pole trójk¹ta
+triangleArea = a*h/2
+
+//alert
+alert('Triangle Area is:' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
